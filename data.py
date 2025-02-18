107b40d0359e48f83a9ea44dd7aacbe0c652f329
Anderson <101288487+Andsu022@users.noreply.github.com>
2025-02-18 12:54:09 -0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,15 @@
+lista_cursos_CSHNB = [
+    'Ciências Biológicas',
+    'Educação do Campo/Ciências da Natureza', 
+    'História', 
+    'Letras', 
+    'Pedagogia', 
+    'Matemática',
+    'Administração', 
+    'Enfermagem', 
+    'Medicina', 
+    'Nutrição', 
+    'Sistemas de Informação' 
+]
+
+dicionario_usuarios = {}
